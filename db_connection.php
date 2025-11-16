6a4dc7185a15bf6ba552c1e3f5f27898b23668d5
amalathas jathurshan <104137121+Jathurshan25@users.noreply.github.com>
2025-11-17 02:59:47 +0530

    db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,1 @@
-<?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "ncc";
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-if (!$conn) {
-    die("Something went wrong;");
-}
-
-?>
